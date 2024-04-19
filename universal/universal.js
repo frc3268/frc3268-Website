e42c3d3ad198e99137b3c8518aaade20f1b75024
2027717 <152230855+2027717@users.noreply.github.com>
2024-04-19 12:43:49 -0700

    testing something

--- universal/universal.js
+++ universal/universal.js
@@ -3,8 +3,8 @@ var navbar = document.getElementById("nav");
 var sticky = navbar.offsetTop;
 window.onscroll = function() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    //navbar.classList.add("sticky")
   } else {
-    navbar.classList.remove("sticky");
+    //navbar.classList.remove("sticky");
   }
 };
