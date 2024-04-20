d996625804bfdb1b09bdd1c9532860feda1fa9e9
2027717 <152230855+2027717@users.noreply.github.com>
2024-04-19 22:12:39 -0700

    proboably doesnt work

--- universal/universal.js
+++ universal/universal.js
@@ -1,10 +1,46 @@
 // While we could use Position:sticky for some browsers, it is not a universal thus, I believe this is the better option
-var navbar = document.getElementById("nav");
-var sticky = navbar.offsetTop;
+const scrollYBeforeLogoDisappears = 70;
+const logo = document.querySelector('#logo');
+
 window.onscroll = function() {
-  if (window.pageYOffset >= sticky) {
-    //navbar.classList.add("sticky")
+  if(!logo) {
+    return;
+  }
+  if (scrollY >= scrollYBeforeLogoDisappears) {
+    logoFadeOut();
   } else {
-    //navbar.classList.remove("sticky");
+    logoFadeIn();
   }
 };
+
+function logoFadeOut() {
+  if(!logo) {
+    return;
+  }
+  let logoOpacity = 1;
+  function fade() {
+    logoOpacity -= 0.01;
+    logo.style.opacity = logoOpacity;
+    if(logoOpacity <= 0) {
+      clearInterval(fade);
+      logo.style.opacity = 0;
+    }
+  }
+  setInterval(fade, 10);
+}
+
+function logoFadeIn() {
+  if(!logo) {
+    return;
+  }
+  let logoOpacity = 0;
+  function fade() {
+    logoOpacity += 0.01;
+    logo.style.opacity = logoOpacity;
+    if(logoOpacity >= 1) {
+      clearInterval(fade);
+      logo.style.opacity = 1;
+    }
+  }
+  setInterval(fade, 10);
+}
